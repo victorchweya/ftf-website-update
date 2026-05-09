f69e360f037f2b6eb037c3de150fafb75cc43eb6
Victor Chweya <vchweya@avicdesign.com>
2026-05-09 06:19:52 +0300

    update headers

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE =
+	"Farm to Feed Kenya - The ODD looking solution to food loss";
+export const SITE_DESCRIPTION =
+	"Since 2021, Farm to Feed - a women-led Kenyan startup - has been closing this gap. We rescue imperfect and surplus produce, extend shelf life, and connect farmers to fair, reliable markets—while offering buyers a seamless platform to source fresh, traceable, and affordable food with confidence.";
